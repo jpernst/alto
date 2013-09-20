aab9593a1bc0b4dab008a29599eea2bff7ee964a
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-09-21 03:17:29 +1000

    Revert to using a statics instead of an enum
    Until we can specify the type of a C-style enum (there is an open PR), this is probably better for now. Sorry!

--- src/openal/al.rs
+++ src/openal/al.rs
@@ -2,161 +2,137 @@ use types::*;
 use ffi;
 use std::{cast, str, sys, uint};
 
+pub static NONE                           : ALenum = 0;
+
 pub static FALSE                          : ALboolean = 0;
 pub static TRUE                           : ALboolean = 1;
 
-#[deriving(Eq, ToStr)]
-pub enum ALenum {
-    None_NoError = 0,
-    SourceRelative = 0x202,
-    ConeInnerAngle = 0x1001,
-    ConeOuterAngle = 0x1002,
-    Pitch = 0x1003,
-    Position = 0x1004,
-    Direction = 0x1005,
-    Velocity = 0x1006,
-    Looping = 0x1007,
-    Buffer = 0x1009,
-    Gain = 0x100A,
-    MinGain = 0x100d,
-    MaxGain = 0x100e,
-    Orientation = 0x100f,
-    SourceState = 0x1010,
-    Initial = 0x1011,
-    Playing = 0x1012,
-    Paused = 0x1013,
-    Stopped = 0x1014,
-    BuffersQueued = 0x1015,
-    BuffersProcessed = 0x1016,
-    SecOffset = 0x1024,
-    SampleOffset = 0x1025,
-    ByteOffset = 0x1026,
-    SourceType = 0x1027,
-    Static = 0x1028,
-    Streaming = 0x1029,
-    Undetermined = 0x1030,
-    FormatMono8 = 0x1100,
-    FormatMono16 = 0x1101,
-    FormatStereo8 = 0x1102,
-    FormatStereo16 = 0x1103,
-    ReferenceDistance = 0x1020,
-    RolloffFactor = 0x1021,
-    ConeOuterGain = 0x1022,
-    MaxDistance = 0x1023,
-    Frequency = 0x2001,
-    Bits = 0x2002,
-    Channels = 0x2003,
-    Size = 0x2004,
-    Unused = 0x2010,
-    Pending = 0x2011,
-    Processed = 0x2012,
-    InvalidName = 0xA001,
-    InvalidEnum = 0xA002,
-    InvalidValue = 0xA003,
-    InvalidOperation = 0xA004,
-    OutOfMemory = 0xA005,
-    Vendor = 0xb001,
-    Version = 0xb002,
-    Renderer = 0xb003,
-    Extensions = 0xb004,
-    DopplerFactor = 0xc000,
-    DopplerVelocity = 0xc001,
-    SpeedofSound = 0xc003,
-    DistanceModel = 0xd000,
-    InverseDistance = 0xd001,
-    InverseDistanceClamped = 0xd002,
-    LinearDistance = 0xd003,
-    LinearDistanceClamped = 0xd004,
-    ExponentDistance = 0xd005,
-    ExponentDistanceClamped = 0xd006
-}
-
-fn uint_to_ALenum(n: uint) -> Option<ALenum> {
-    unsafe {
-        match n {
-            1u .. 513u => None,
-            515u .. 4096u => None,
-            4104u => None,
-            4107u .. 4108u => None,
-            4119u .. 4127u => None,
-            4138u .. 4143u => None,
-            4145u .. 4351u => None,
-            4356u .. 8192u => None,
-            8197u .. 8207u => None,
-            8211u .. 40960u => None,
-            40966u .. 45056u => None,
-            45061u .. 49151u => None,
-            49154u => None,
-            49156u .. 53247u => None,
-            53255u .. uint::max_value => None,
-            _ => Some(cast::transmute(n as uint)),
-        }
-    }
-}
+pub static SOURCE_RELATIVE                : ALenum = 0x202;
+pub static CONE_INNER_ANGLE               : ALenum = 0x1001;
+pub static CONE_OUTER_ANGLE               : ALenum = 0x1002;
+pub static PITCH                          : ALenum = 0x1003;
+pub static POSITION                       : ALenum = 0x1004;
+pub static DIRECTION                      : ALenum = 0x1005;
+pub static VELOCITY                       : ALenum = 0x1006;
+pub static LOOPING                        : ALenum = 0x1007;
+pub static BUFFER                         : ALenum = 0x1009;
+pub static GAIN                           : ALenum = 0x100A;
+pub static MIN_GAIN                       : ALenum = 0x100D;
+pub static MAX_GAIN                       : ALenum = 0x100E;
+pub static ORIENTATION                    : ALenum = 0x100F;
+pub static SOURCE_STATE                   : ALenum = 0x1010;
+pub static INITIAL                        : ALenum = 0x1011;
+pub static PLAYING                        : ALenum = 0x1012;
+pub static PAUSED                         : ALenum = 0x1013;
+pub static STOPPED                        : ALenum = 0x1014;
+pub static BUFFERS_QUEUED                 : ALenum = 0x1015;
+pub static BUFFERS_PROCESSED              : ALenum = 0x1016;
+pub static SEC_OFFSET                     : ALenum = 0x1024;
+pub static SAMPLE_OFFSET                  : ALenum = 0x1025;
+pub static BYTE_OFFSET                    : ALenum = 0x1026;
+pub static SOURCE_TYPE                    : ALenum = 0x1027;
+pub static STATIC                         : ALenum = 0x1028;
+pub static STREAMING                      : ALenum = 0x1029;
+pub static UNDETERMINED                   : ALenum = 0x1030;
+pub static FORMAT_MONO8                   : ALenum = 0x1100;
+pub static FORMAT_MONO16                  : ALenum = 0x1101;
+pub static FORMAT_STEREO8                 : ALenum = 0x1102;
+pub static FORMAT_STEREO16                : ALenum = 0x1103;
+pub static REFERENCE_DISTANCE             : ALenum = 0x1020;
+pub static ROLLOFF_FACTOR                 : ALenum = 0x1021;
+pub static CONE_OUTER_GAIN                : ALenum = 0x1022;
+pub static MAX_DISTANCE                   : ALenum = 0x1023;
+pub static FREQUENCY                      : ALenum = 0x2001;
+pub static BITS                           : ALenum = 0x2002;
+pub static CHANNELS                       : ALenum = 0x2003;
+pub static SIZE                           : ALenum = 0x2004;
+pub static UNUSED                         : ALenum = 0x2010;
+pub static PENDING                        : ALenum = 0x2011;
+pub static PROCESSED                      : ALenum = 0x2012;
+pub static NO_ERROR                       : ALenum = FALSE as ALenum;
+pub static INVALID_NAME                   : ALenum = 0xA001;
+pub static INVALID_ENUM                   : ALenum = 0xA002;
+pub static INVALID_VALUE                  : ALenum = 0xA003;
+pub static INVALID_OPERATION              : ALenum = 0xA004;
+pub static OUT_OF_MEMORY                  : ALenum = 0xA005;
+pub static VENDOR                         : ALenum = 0xB001;
+pub static VERSION                        : ALenum = 0xB002;
+pub static RENDERER                       : ALenum = 0xB003;
+pub static EXTENSIONS                     : ALenum = 0xB004;
+pub static DOPPLER_FACTOR                 : ALenum = 0xC000;
+pub static DOPPLER_VELOCITY               : ALenum = 0xC001;
+pub static SPEED_OF_SOUND                 : ALenum = 0xC003;
+pub static DISTANCE_MODEL                 : ALenum = 0xD000;
+pub static INVERSE_DISTANCE               : ALenum = 0xD001;
+pub static INVERSE_DISTANCE_CLAMPED       : ALenum = 0xD002;
+pub static LINEAR_DISTANCE                : ALenum = 0xD003;
+pub static LINEAR_DISTANCE_CLAMPED        : ALenum = 0xD004;
+pub static EXPONENT_DISTANCE              : ALenum = 0xD005;
+pub static EXPONENT_DISTANCE_CLAMPED      : ALenum = 0xD006;
 
 #[fixed_stack_segment]
 pub fn enable(capability: ALenum) {
-    unsafe { ffi::alEnable(capability as i32); }
+    unsafe { ffi::alEnable(capability); }
 }
 
 #[fixed_stack_segment]
 pub fn disable(capability: ALenum) {
-    unsafe { ffi::alDisable(capability as i32); }
+    unsafe { ffi::alDisable(capability); }
 }
 
 #[fixed_stack_segment]
 pub fn is_enabled(capability: ALenum) -> ALboolean {
-    unsafe { ffi::alIsEnabled(capability as i32) }
+    unsafe { ffi::alIsEnabled(capability) }
 }
 
 #[fixed_stack_segment]
 pub fn get_string(param: ALenum) -> ~str {
-    unsafe { str::raw::from_c_str(ffi::alGetString(param as i32)) }
+    unsafe { str::raw::from_c_str(ffi::alGetString(param)) }
 }
 
 #[fixed_stack_segment]
 pub fn get_booleanv(param: ALenum, data: &mut ALboolean) {
-    unsafe { ffi::alGetBooleanv(param as i32, cast::transmute(data)); }
+    unsafe { ffi::alGetBooleanv(param, cast::transmute(data)); }
 }
 
 #[fixed_stack_segment]
 pub fn get_integerv(param: ALenum, data: &mut ALint) {
-    unsafe { ffi::alGetIntegerv(param as i32, cast::transmute(data)); }
+    unsafe { ffi::alGetIntegerv(param, cast::transmute(data)); }
 }
 
 #[fixed_stack_segment]
 pub fn get_floatv(param: ALenum, data: &mut ALfloat) {
-    unsafe { ffi::alGetFloatv(param as i32, cast::transmute(data)); }
+    unsafe { ffi::alGetFloatv(param, cast::transmute(data)); }
 }
 
 #[fixed_stack_segment]
 pub fn get_doublev(param: ALenum, data: &mut ALdouble) {
-    unsafe { ffi::alGetDoublev(param as i32, cast::transmute(data)); }
+    unsafe { ffi::alGetDoublev(param, cast::transmute(data)); }
 }
 
 #[fixed_stack_segment]
 pub fn get_boolean(param: ALenum) -> ALboolean {
-    unsafe { ffi::alGetBoolean(param as i32) }
+    unsafe { ffi::alGetBoolean(param) }
 }
 
 #[fixed_stack_segment]
 pub fn get_integer(param: ALenum) -> ALint {
-    unsafe { ffi::alGetInteger(param as i32) }
+    unsafe { ffi::alGetInteger(param) }
 }
 
 #[fixed_stack_segment]
 pub fn get_float(param: ALenum) -> ALfloat {
-    unsafe { ffi::alGetFloat(param as i32) }
+    unsafe { ffi::alGetFloat(param) }
 }
 
 #[fixed_stack_segment]
 pub fn get_double(param: ALenum) -> ALdouble {
-    unsafe { ffi::alGetDouble(param as i32) }
+    unsafe { ffi::alGetDouble(param) }
 }
 
 #[fixed_stack_segment]
 pub fn get_error() -> ALenum {
-    unsafe { uint_to_ALenum(ffi::alGetError() as uint).unwrap() }
+    unsafe { ffi::alGetError() }
 }
 
 #[fixed_stack_segment]
@@ -173,18 +149,18 @@ pub fn get_proc_address(fname: &str) -> extern fn() {
 
 #[fixed_stack_segment]
 pub fn get_enum_value(ename: &str) -> ALenum {
-    unsafe { uint_to_ALenum(ffi::alGetEnumValue( ename.with_c_str( |s| s)) as uint).unwrap() }
+    unsafe { ffi::alGetEnumValue( ename.with_c_str( |s| s)) }
 }
 
 
 #[fixed_stack_segment]
 pub fn listenerf(param: ALenum, value: ALfloat) {
-    unsafe { ffi::alListenerf(param as i32, value); }
+    unsafe { ffi::alListenerf(param, value); }
 }
 
 #[fixed_stack_segment]
 pub fn listener3f(param: ALenum, value1: ALfloat, value2: ALfloat, value3: ALfloat) {
-    unsafe { ffi::alListener3f(param as i32, value1, value2, value3); }
+    unsafe { ffi::alListener3f(param, value1, value2, value3); }
 }
 
 // #[fixed_stack_segment]
@@ -194,12 +170,12 @@ pub fn listener3f(param: ALenum, value1: ALfloat, value2: ALfloat, value3: ALflo
 
 #[fixed_stack_segment]
 pub fn listeneri(param: ALenum, value: ALint) {
-    unsafe { ffi::alListeneri(param as i32, value); }
+    unsafe { ffi::alListeneri(param, value); }
 }
 
 #[fixed_stack_segment]
 pub fn listener3i(param: ALenum, value1: ALint, value2: ALint, value3: ALint) {
-    unsafe { ffi::alListener3i(param as i32, value1, value2, value3); }
+    unsafe { ffi::alListener3i(param, value1, value2, value3); }
 }
 
 // pub fn Listeneriv(param: ALenum, values: *ALint) {
@@ -209,7 +185,7 @@ pub fn listener3i(param: ALenum, value1: ALint, value2: ALint, value3: ALint) {
 #[fixed_stack_segment]
 pub fn get_listenerf(param: ALenum) -> ALfloat {
     let mut value = 0.0;
-    unsafe { ffi::alGetListenerf(param as i32, cast::transmute(&value)) }
+    unsafe { ffi::alGetListenerf(param, cast::transmute(&value)) }
     value
 }
 
@@ -218,7 +194,7 @@ pub fn get_listener3f(param: ALenum) -> (ALfloat, ALfloat, ALfloat) {
     let mut value1 = 0.0;
     let mut value2 = 0.0;
     let mut value3 = 0.0;
-    unsafe { ffi::alGetListener3f(param as i32, cast::transmute(&value1),
+    unsafe { ffi::alGetListener3f(param, cast::transmute(&value1),
                                         cast::transmute(&value2),
                                         cast::transmute(&value3)) }
     (value1, value2, value3)
@@ -232,7 +208,7 @@ pub fn get_listener3f(param: ALenum) -> (ALfloat, ALfloat, ALfloat) {
 #[fixed_stack_segment]
 pub fn get_listeneri(param: ALenum) -> ALint {
     let mut value = 0;
-    unsafe { ffi::alGetListeneri(param as i32, cast::transmute(&value)) }
+    unsafe { ffi::alGetListeneri(param, cast::transmute(&value)) }
     value
 }
 
@@ -241,7 +217,7 @@ pub fn get_listener3i(param: ALenum) -> (ALint, ALint, ALint) {
     let mut value1 = 0;
     let mut value2 = 0;
     let mut value3 = 0;
-    unsafe { ffi::alGetListener3i(param as i32, cast::transmute(&value1),
+    unsafe { ffi::alGetListener3i(param, cast::transmute(&value1),
                                         cast::transmute(&value2),
                                         cast::transmute(&value3)) }
     (value1, value2, value3)
@@ -269,12 +245,12 @@ pub fn is_source(sid: ALuint) -> ALboolean {
 
 #[fixed_stack_segment]
 pub fn sourcef(sid: ALuint, param: ALenum, value: ALfloat) {
-    unsafe { ffi::alSourcef(sid, param as i32, value); }
+    unsafe { ffi::alSourcef(sid, param, value); }
 }
 
 #[fixed_stack_segment]
 pub fn source3f(sid: ALuint, param: ALenum, value1: ALfloat, value2: ALfloat, value3: ALfloat) {
-    unsafe { ffi::alSource3f(sid, param as i32, value1, value2, value3); }
+    unsafe { ffi::alSource3f(sid, param, value1, value2, value3); }
 }
 
 // #[fixed_stack_segment]
@@ -284,12 +260,12 @@ pub fn source3f(sid: ALuint, param: ALenum, value1: ALfloat, value2: ALfloat, va
 
 #[fixed_stack_segment]
 pub fn sourcei(sid: ALuint, param: ALenum, value: ALint) {
-    unsafe { ffi::alSourcei(sid, param as i32, value); }
+    unsafe { ffi::alSourcei(sid, param, value); }
 }
 
 #[fixed_stack_segment]
 pub fn source3i(sid: ALuint, param: ALenum, value1: ALint, value2: ALint, value3: ALint) {
-    unsafe { ffi::alSource3i(sid, param as i32, value1, value2, value3); }
+    unsafe { ffi::alSource3i(sid, param, value1, value2, value3); }
 }
 
 // #[fixed_stack_segment]
@@ -300,7 +276,7 @@ pub fn source3i(sid: ALuint, param: ALenum, value1: ALint, value2: ALint, value3
 #[fixed_stack_segment]
 pub fn get_sourcef(sid: ALuint, param: ALenum) -> ALfloat {
     let mut value = 0.0;
-    unsafe { ffi::alGetSourcef(sid, param as i32, cast::transmute(&value)); }
+    unsafe { ffi::alGetSourcef(sid, param, cast::transmute(&value)); }
     value
 }
 
@@ -309,7 +285,7 @@ pub fn get_source3f(sid: ALuint, param: ALenum) -> (ALfloat, ALfloat, ALfloat) {
     let mut value1 = 0.0;
     let mut value2 = 0.0;
     let mut value3 = 0.0;
-    unsafe { ffi::alGetSource3f(sid, param as i32, cast::transmute(&value1),
+    unsafe { ffi::alGetSource3f(sid, param, cast::transmute(&value1),
                                            cast::transmute(&value2),
                                            cast::transmute(&value3)); }
     (value1, value2, value3)
@@ -323,7 +299,7 @@ pub fn get_source3f(sid: ALuint, param: ALenum) -> (ALfloat, ALfloat, ALfloat) {
 #[fixed_stack_segment]
 pub fn get_sourcei(sid: ALuint,  param: ALenum) -> ALint {
     let mut value = 0;
-    unsafe { ffi::alGetSourcei(sid, param as i32, cast::transmute(&value)); }
+    unsafe { ffi::alGetSourcei(sid, param, cast::transmute(&value)); }
     value
 }
 
@@ -332,7 +308,7 @@ pub fn get_source3i(sid: ALuint, param: ALenum) -> (ALint, ALint, ALint) {
     let mut value1 = 0;
     let mut value2 = 0;
     let mut value3 = 0;
-    unsafe { ffi::alGetSource3i(sid, param as i32, cast::transmute(&value1),
+    unsafe { ffi::alGetSource3i(sid, param, cast::transmute(&value1),
                                            cast::transmute(&value2),
                                            cast::transmute(&value3)); }
     (value1, value2, value3)
@@ -412,7 +388,7 @@ pub fn is_buffer(bid: ALuint) -> ALboolean {
 pub fn buffer_data<T>(bid: ALuint, format: ALenum, data: &[T], freq: ALsizei) {
     unsafe {
         ffi::alBufferData(
-            bid, format as i32,
+            bid, format,
             cast::transmute(&data[0]),
             sys::size_of::<T>() as ALsizei * data.len() as ALsizei,
             freq
@@ -422,12 +398,12 @@ pub fn buffer_data<T>(bid: ALuint, format: ALenum, data: &[T], freq: ALsizei) {
 
 #[fixed_stack_segment]
 pub fn bufferf(bid: ALuint, param: ALenum, value: ALfloat) {
-    unsafe { ffi::alBufferf(bid, param as i32, value); }
+    unsafe { ffi::alBufferf(bid, param, value); }
 }
 
 #[fixed_stack_segment]
 pub fn buffer3f(bid: ALuint, param: ALenum, value1: ALfloat, value2: ALfloat, value3: ALfloat) {
-    unsafe { ffi::alBuffer3f(bid, param as i32, value1, value2, value3); }
+    unsafe { ffi::alBuffer3f(bid, param, value1, value2, value3); }
 }
 
 // #[fixed_stack_segment]
@@ -437,12 +413,12 @@ pub fn buffer3f(bid: ALuint, param: ALenum, value1: ALfloat, value2: ALfloat, va
 
 #[fixed_stack_segment]
 pub fn bufferi(bid: ALuint, param: ALenum, value: ALint) {
-    unsafe { ffi::alBufferi(bid, param as i32, value); }
+    unsafe { ffi::alBufferi(bid, param, value); }
 }
 
 #[fixed_stack_segment]
 pub fn buffer3i(bid: ALuint, param: ALenum, value1: ALint, value2: ALint, value3: ALint) {
-    unsafe { ffi::alBuffer3i(bid, param as i32, value1, value2, value3); }
+    unsafe { ffi::alBuffer3i(bid, param, value1, value2, value3); }
 }
 
 // #[fixed_stack_segment]
@@ -453,7 +429,7 @@ pub fn buffer3i(bid: ALuint, param: ALenum, value1: ALint, value2: ALint, value3
 #[fixed_stack_segment]
 pub fn get_bufferf(sid: ALuint, param: ALenum) -> ALfloat {
     let mut value = 0.0;
-    unsafe { ffi::alGetBufferf(sid, param as i32, cast::transmute(&value)); }
+    unsafe { ffi::alGetBufferf(sid, param, cast::transmute(&value)); }
     value
 }
 
@@ -462,7 +438,7 @@ pub fn get_buffer3f(sid: ALuint, param: ALenum) -> (ALfloat, ALfloat, ALfloat) {
     let mut value1 = 0.0;
     let mut value2 = 0.0;
     let mut value3 = 0.0;
-    unsafe { ffi::alGetBuffer3f(sid, param as i32, cast::transmute(&value1),
+    unsafe { ffi::alGetBuffer3f(sid, param, cast::transmute(&value1),
                                            cast::transmute(&value2),
                                            cast::transmute(&value3)); }
     (value1, value2, value3)
@@ -476,7 +452,7 @@ pub fn get_buffer3f(sid: ALuint, param: ALenum) -> (ALfloat, ALfloat, ALfloat) {
 #[fixed_stack_segment]
 pub fn get_bufferi(sid: ALuint,  param: ALenum) -> ALint {
     let mut value = 0;
-    unsafe { ffi::alGetBufferi(sid, param as i32, cast::transmute(&value)); }
+    unsafe { ffi::alGetBufferi(sid, param, cast::transmute(&value)); }
     value
 }
 
@@ -485,7 +461,7 @@ pub fn get_buffer3i(sid: ALuint, param: ALenum) -> (ALint, ALint, ALint) {
     let mut value1 = 0;
     let mut value2 = 0;
     let mut value3 = 0;
-    unsafe { ffi::alGetBuffer3i(sid, param as i32, cast::transmute(&value1),
+    unsafe { ffi::alGetBuffer3i(sid, param, cast::transmute(&value1),
                                            cast::transmute(&value2),
                                            cast::transmute(&value3)); }
     (value1, value2, value3)

d081500d0a876ad8de89bde3b7b06dfb970f9ce4
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-09-21 04:21:58 +1000

    Move group extern fns and types into the al and alc modules

--- src/openal/alc.rs
+++ src/openal/alc.rs
@@ -1,8 +1,54 @@
 use std::cast;
 use std::vec;
 
-use ffi;
-use types::*;
+use self::types::*;
+
+pub mod types {
+    use std::libc::*;
+    pub type ALCboolean             = c_char;
+    pub type ALCchar                = c_char;
+    pub type ALCbyte                = c_char;
+    pub type ALCubyte               = c_uchar;
+    pub type ALCshort               = c_short;
+    pub type ALCushort              = c_ushort;
+    pub type ALCint                 = c_int;
+    pub type ALCuint                = c_uint;
+    pub type ALCsizei               = c_int;
+    pub type ALCenum                = c_int;
+    pub type ALCfloat               = c_float;
+    pub type ALCdouble              = c_double;
+    pub type ALCvoid                = c_void;
+}
+
+pub mod ffi {
+    use super::types::*;
+
+    pub struct ALCdevice;
+    pub struct ALCcontext;
+
+    extern "C" {
+        pub fn alcCreateContext(device: *ALCdevice, attrlist: *ALCint) -> *ALCcontext;
+        pub fn alcMakeContextCurrent(context: *ALCcontext) -> ALCboolean;
+        pub fn alcProcessContext(context: *ALCcontext);
+        pub fn alcSuspendContext(context: *ALCcontext);
+        pub fn alcDestroyContext(context: *ALCcontext);
+        pub fn alcGetCurrentContext() -> *ALCcontext;
+        pub fn alcGetContextsDevice(context: *ALCcontext) -> *ALCdevice;
+        pub fn alcOpenDevice(devicename: *ALCchar) -> *ALCdevice;
+        pub fn alcCloseDevice(device: *ALCdevice) -> ALCboolean;
+        pub fn alcGetError(device: *ALCdevice) -> ALCenum;
+        pub fn alcIsExtensionPresent(device: *ALCdevice, extname: *ALCchar) -> ALCboolean;
+        pub fn alcGetProcAddress(device: *ALCdevice, funcname: *ALCchar) -> Option<extern "C" fn()>;
+        pub fn alcGetEnumValue(device: *ALCdevice, enumname: *ALCchar) -> ALCenum;
+        pub fn alcGetString(device: *ALCdevice, param: ALCenum) -> *ALCchar;
+        pub fn alcGetIntegerv(device: *ALCdevice, param: ALCenum, size: ALCsizei, data: *ALCint);
+        pub fn alcCaptureOpenDevice(devicename: *ALCchar, frequency: ALCuint, format: ALCenum, buffersize: ALCsizei) -> *ALCdevice;
+        pub fn alcCaptureCloseDevice(device: *ALCdevice) -> ALCboolean;
+        pub fn alcCaptureStart(device: *ALCdevice);
+        pub fn alcCaptureStop(device: *ALCdevice);
+        pub fn alcCaptureSamples(device: *ALCdevice, buffer: *ALCvoid, samples: ALCsizei);
+    }
+}
 
 // TODO: not sure what types these are meant to be...
 pub static INVALID                              : ALCboolean = 0;
@@ -38,104 +84,104 @@ pub static CAPTURE_DEFAULT_DEVICE_SPECIFIER     : ALCenum = 0x311;
 pub static CAPTURE_SAMPLES                      : ALCenum = 0x312;
 
 #[fixed_stack_segment]
-pub fn create_context(device: *ALCdevice, attrlist: &[ALCint]) -> *ALCcontext {
+pub fn create_context(device: *ffi::ALCdevice, attrlist: &[ALCint]) -> *ffi::ALCcontext {
     let attrs_terminated = vec::append_one(attrlist.to_owned(), 0);  // teminate attributes with a 0
     unsafe { ffi::alcCreateContext(device, cast::transmute(&attrs_terminated[0])) }
 }
 
 #[fixed_stack_segment]
-pub fn make_context_current(context: *ALCcontext) -> ALboolean {
+pub fn make_context_current(context: *ffi::ALCcontext) -> ALCboolean {
     unsafe { ffi::alcMakeContextCurrent(context) }
 }
 
 #[fixed_stack_segment]
-pub fn process_context(context: *ALCcontext) {
+pub fn process_context(context: *ffi::ALCcontext) {
     unsafe { ffi::alcProcessContext(context); }
 }
 
 #[fixed_stack_segment]
-pub fn suspend_context(context: *ALCcontext) {
+pub fn suspend_context(context: *ffi::ALCcontext) {
     unsafe { ffi::alcSuspendContext(context); }
 }
 
 #[fixed_stack_segment]
-pub fn destroy_context(context: *ALCcontext) {
+pub fn destroy_context(context: *ffi::ALCcontext) {
     unsafe { ffi::alcDestroyContext(context); }
 }
 
 #[fixed_stack_segment]
-pub fn get_current_context() -> *ALCcontext {
+pub fn get_current_context() -> *ffi::ALCcontext {
     unsafe { ffi::alcGetCurrentContext() }
 }
 
 #[fixed_stack_segment]
-pub fn get_contexts_device(context: *ALCcontext) -> *ALCdevice {
+pub fn get_contexts_device(context: *ffi::ALCcontext) -> *ffi::ALCdevice {
     unsafe { ffi::alcGetContextsDevice(context) }
 }
 
 #[fixed_stack_segment]
-pub fn open_device(devicename: &str) -> *ALCdevice {
+pub fn open_device(devicename: &str) -> *ffi::ALCdevice {
     unsafe { devicename.with_c_str(|c_str| ffi::alcOpenDevice(c_str)) }
 }
 
 #[fixed_stack_segment]
-pub fn close_device(device: *ALCdevice) -> ALboolean {
+pub fn close_device(device: *ffi::ALCdevice) -> ALCboolean {
     unsafe { ffi::alcCloseDevice(device) }
 }
 
 #[fixed_stack_segment]
-pub fn get_error(device: *ALCdevice) -> ALCenum {
+pub fn get_error(device: *ffi::ALCdevice) -> ALCenum {
     unsafe { ffi::alcGetError(device) }
 }
 
 #[fixed_stack_segment]
-pub fn is_extension_present(device: *ALCdevice, extname: &str) -> ALboolean {
+pub fn is_extension_present(device: *ffi::ALCdevice, extname: &str) -> ALCboolean {
     unsafe { extname.with_c_str(|c_str| ffi::alcIsExtensionPresent(device, c_str)) }
 }
 
 #[fixed_stack_segment]
-pub fn get_proc_address(device: *ALCdevice, funcname: ~str) -> extern "C" fn() {
+pub fn get_proc_address(device: *ffi::ALCdevice, funcname: ~str) -> extern "C" fn() {
     unsafe { cast::transmute(
         funcname.with_c_str(|c_str| ffi::alcGetProcAddress(device, c_str))
     ) }
 }
 
 #[fixed_stack_segment]
-pub fn get_enum_value(device: *ALCdevice, enumname: &str) -> ALCenum {
+pub fn get_enum_value(device: *ffi::ALCdevice, enumname: &str) -> ALCenum {
     unsafe { enumname.with_c_str(|c_str| ffi::alcGetEnumValue(device, c_str)) }
 }
 
 // #[fixed_stack_segment]
-// pub fn get_string(device: *ALCdevice, param: ALCenum) -> *ALCchar {
+// pub fn get_string(device: *ffi::ALCdevice, param: ALCenum) -> *ALCchar {
 //     unsafe { ffi::alcGetString(device, param) }
 // }
 
 // #[fixed_stack_segment]
-// pub fn GetIntegerv(device: *ALCdevice, param: ALCenum, size: ALCsizei, data: *ALCint) {
+// pub fn GetIntegerv(device: *ffi::ALCdevice, param: ALCenum, size: ALCsizei, data: *ALCint) {
 //     unsafe { ffi::alcGetIntegerv(); }
 // }
 
 #[fixed_stack_segment]
-pub fn capture_open_device(devicename: *ALCchar, frequency: ALCuint, format: ALCenum, buffersize: ALCsizei) -> *ALCdevice {
+pub fn capture_open_device(devicename: *ALCchar, frequency: ALCuint, format: ALCenum, buffersize: ALCsizei) -> *ffi::ALCdevice {
     unsafe { ffi::alcCaptureOpenDevice(devicename, frequency, format, buffersize) }
 }
 
 #[fixed_stack_segment]
-pub fn capture_close_device(device: *ALCdevice) -> ALboolean {
+pub fn capture_close_device(device: *ffi::ALCdevice) -> ALCboolean {
     unsafe { ffi::alcCaptureCloseDevice(device) }
 }
 
 #[fixed_stack_segment]
-pub fn capture_start(device: *ALCdevice) {
+pub fn capture_start(device: *ffi::ALCdevice) {
     unsafe { ffi::alcCaptureStart(device); }
 }
 
 #[fixed_stack_segment]
-pub fn capture_stop(device: *ALCdevice) {
+pub fn capture_stop(device: *ffi::ALCdevice) {
     unsafe { ffi::alcCaptureStop(device); }
 }
 
 // #[fixed_stack_segment]
-// pub fn CaptureSamples(device: *ALCdevice, buffer: *ALCvoid, samples: ALCsizei) {
+// pub fn CaptureSamples(device: *ffi::ALCdevice, buffer: *ALCvoid, samples: ALCsizei) {
 //     unsafe { ffi::alcCaptureSamples(); }
 // }
--- src/openal/lib.rs
+++ src/openal/lib.rs
@@ -7,42 +7,15 @@
 #[comment = "OpenAL 1.1 bindings for Rust."];
 #[crate_type = "lib"];
 
-pub mod ffi;
 pub mod al;
 pub mod alc;
 
-/// Core OpenAL typedefs
-pub mod types {
-    use std::libc::*;
+#[nolink]
+#[link_args="-framework OpenAL"]
+#[cfg(target_os = "macos")]
+extern {}
 
-    pub type ALboolean              = c_char;
-    pub type ALchar                 = c_char;
-    pub type ALbyte                 = c_char;
-    pub type ALubyte                = c_uchar;
-    pub type ALshort                = c_short;
-    pub type ALushort               = c_ushort;
-    pub type ALint                  = c_int;
-    pub type ALuint                 = c_uint;
-    pub type ALsizei                = c_int;
-    pub type ALenum                 = c_int;
-    pub type ALfloat                = c_float;
-    pub type ALdouble               = c_double;
-    pub type ALvoid                 = c_void;
-
-    pub struct ALCdevice;
-    pub struct ALCcontext;
-
-    pub type ALCboolean             = c_char;
-    pub type ALCchar                = c_char;
-    pub type ALCbyte                = c_char;
-    pub type ALCubyte               = c_uchar;
-    pub type ALCshort               = c_short;
-    pub type ALCushort              = c_ushort;
-    pub type ALCint                 = c_int;
-    pub type ALCuint                = c_uint;
-    pub type ALCsizei               = c_int;
-    pub type ALCenum                = c_int;
-    pub type ALCfloat               = c_float;
-    pub type ALCdouble              = c_double;
-    pub type ALCvoid                = c_void;
-}
+#[nolink]
+#[link_args="-lopenal"]
+#[cfg(target_os = "linux")]
+extern {}

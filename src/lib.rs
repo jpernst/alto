8c5d60053e586c4ba53d1db900976290297725f3
Jameson Ernst <j.patrick.ernst@gmail.com>
2016-10-29 19:28:22 -0700

    Start on high-level wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,30 @@
+use std::marker::PhantomData;
+
 mod al;
 mod alc;
 mod alext;
 mod efx_presets;
+
+
+
+pub mod ffi {
+	pub use al::*;
+	pub use alc::*;
+	pub use alext::*;
+	pub use efx_presets::*;
+}
+
+
+
+pub struct Device(*mut alc::ALCdevice);
+pub struct Context<'d>(*mut alc::ALCcontext, PhantomData<&'d alc::ALCdevice>);
+
+
+
+impl Device {
+}
+
+
+
+impl<'d> Context<'d> {
+}
